d5c77a61f74569080808bd498d8b882dfc2bf3ac
박성범 <shivash@naver.com>
2020-08-03 16:22:25 +0900

    add README

---
+++ src/main/java/spring/study/kim/hellospringpsb/HelloSpringPsbApplication.java
@@ -0,0 +1,13 @@
+package spring.study.kim.hellospringpsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringPsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringPsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/study/kim/hellospringpsb/HelloSpringPsbApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.study.kim.hellospringpsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringPsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
