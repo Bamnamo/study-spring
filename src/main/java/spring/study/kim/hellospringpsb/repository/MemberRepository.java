d03964b5a5ab455390804640d829286ec7069132
박성범 <shivash@naver.com>
2020-08-03 16:36:41 +0900

    test code

---
+++ src/main/java/spring/study/kim/hellospringpsb/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package spring.study.kim.hellospringpsb.repository;
+
+import spring.study.kim.hellospringpsb.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
