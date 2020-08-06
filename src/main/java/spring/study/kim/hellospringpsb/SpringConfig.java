b496b30134e150ccc43e65ca9cf2c65aeb23ab5a
박성범 <shivash@naver.com>
2020-08-06 15:51:29 +0900

    Spring 통합 테스트

--- src/main/java/spring/study/kim/hellospringpsb/SpringConfig.java
+++ src/main/java/spring/study/kim/hellospringpsb/SpringConfig.java
@@ -1,20 +1,30 @@
 package spring.study.kim.hellospringpsb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import spring.study.kim.hellospringpsb.repository.MemoryMemberRepository;
+import spring.study.kim.hellospringpsb.repository.JdbcMemberRepository;
 import spring.study.kim.hellospringpsb.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memoryMemberRepository());
     }
 
     @Bean
-    public MemoryMemberRepository memoryMemberRepository(){
-        return new MemoryMemberRepository();
+    public JdbcMemberRepository memoryMemberRepository() {
+        return new JdbcMemberRepository(dataSource);
     }
 }
